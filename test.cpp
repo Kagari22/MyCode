22a0304d0ae38473e7eab7536f0cc181a99ee338
Kagari22 <151036819+Kagari22@users.noreply.github.com>
2025-03-11 18:37:03 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+// from: cpp.json
+#define INF 2e18
+#define int long long
+using namespace std;
+
+void solve() {
+    
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
