ff9418f902e4cdc32114ad2650e0c1b38bf91b71
Kagari22 <151036819+Kagari22@users.noreply.github.com>
2025-10-20 22:42:28 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 
 // from: cpp.json
-#define INF 2e18
+#define INF 8e18
+#define LD long double
+#define i128 __int128_t
 #define int long long
 using namespace std;
 
@@ -13,7 +15,12 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    solve();
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
 
     return 0;
 }
